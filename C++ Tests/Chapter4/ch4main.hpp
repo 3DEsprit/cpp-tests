3e074475edb2309ded403b9d05d1d3b9173f0030
Dennis Brown <vilesyn@gmail.com>
2019-08-29 18:44:39 -0500

    update chapter 4 main header

---
+++ C++ Tests/Chapter4/ch4main.hpp
@@ -0,0 +1,20 @@
+//
+//  ch4main.hpp
+//  C++ Tests
+//
+//  Created by Dennis on 2019/08/25.
+//  Copyright © 2019 Dennis. All rights reserved.
+//
+
+#ifndef ch4main_hpp
+#define ch4main_hpp
+
+#include <stdio.h>
+
+#include "ch4notes.cpp"
+#include "ch4example1.cpp"
+
+#endif /* ch4main_hpp */
+
+int ch4notes();
+int ch4example1();
