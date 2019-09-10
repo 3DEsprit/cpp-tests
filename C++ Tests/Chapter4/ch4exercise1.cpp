9da376c44e502b45e476974186dee45c5051d5f2
Dennis Brown <vilesyn@gmail.com>
2019-09-09 21:26:05 -0500

    chapter 4 exercise 1

--- C++ Tests/Chapter4/ch4exercise1.cpp
+++ C++ Tests/Chapter4/ch4exercise1.cpp
@@ -7,10 +7,22 @@
 //
 
 #include <stdio.h>
+#include <iostream>
 
 int ch4exercise1() {
+    int int_one, int_two;
+    
     // prompt for two integers
+    std::cout << "Enter two integers: ";
+    std::cin >> int_one >> int_two;
+    
     // use if-else that states whether the integers
     // are the same
+    std::cout << "Integers " << int_one << " and " << int_two << " are the ";
+    if(int_one == int_two) {
+        std::cout << "same." << std::endl;
+    } else {
+        std::cout << "different." << std::endl;
+    }
     return 0;
 }
