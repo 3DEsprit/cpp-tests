c9d5e38e11197a06a06d7870d487c9823981bcc2
Dennis Brown <vilesyn@gmail.com>
2019-10-04 18:14:32 -0500

    update chapter 6 notes

--- C++ Tests/Chapter6/ch6notes.cpp
+++ C++ Tests/Chapter6/ch6notes.cpp
@@ -109,10 +109,10 @@ int ch6notes() {
     pvalue = new double { 3.14 };
     
     // allocate initialize and add pointer
-    double* pvalue2 { new double {3.14} };
+    double* _pvalue2 { new double {3.14} };
     
     // allocate initialize and point to double with 0.0
-    double* pvalue3 { new double {} };
+    double* _pvalue3 { new double {} };
     
     // release a value
     delete pvalue;
@@ -122,13 +122,98 @@ int ch6notes() {
     
     // dynamic allocation of arrays
     double* data {new double[100]};
-    int* one_two_three{new int[3] {1, 2, 3}};
-    float* fdata{new float[20] {.1f, .2f}};
+    int* _one_two_three{new int[3] {1, 2, 3}};
+    float* _fdata{new float[20] {.1f, .2f}};
 //    int* one_two_three {new int[] {1, 2, 3}}; // won't work
     
     // remove array from free store
     delete[] data;
     data = nullptr;
+    
+    // allocate vector in free store
+//    auto* pdata {new std::vector<int>()};
+    
+    // or better yet
+    std::vector<int> vec_data;
+    auto* pdata = &vec_data;
+    
+    // add data with defreference
+    (*pdata).push_back(66);
+    
+    // another way of doing the above using
+    // a indirect member selection operator
+    // aka arrow operator
+    pdata->push_back(63);
+    
+    // compiler cannot distinguish between these two
+    int* _single_int{ new int{ 123} };
+    int* _array_of_ints{ new int[123] };
+    
+    // so when deleting this will work
+    // and cause a possible leak
+//    delete[] _single_int;
+//    delete _array_of_ints;
+    
+    // what should be done
+    delete _single_int;
+    delete[] _array_of_ints;
+    
+    // smart pointers
+    
+    // unique_ptr<T>
+    // old style
+    std::unique_ptr<double> _pdata {new double{999.0}};
+    
+    // C++ 14+ style with type
+    std::unique_ptr<double> _pdata2 { std::make_unique<double>(999.0) };
+    // or
+    auto pdata3 { std::make_unique<double>(999.0) };
+    
+    // no longer need to worry about deleting free store
+    *pdata3 = 8888.0;
+    std::cout << *pdata3 << std::endl;
+    
+    // get address with get function
+    std::cout << std::hex << std::showbase << pdata3.get() << std::endl;
+    
+    // unique to array
+    const size_t n { 100 };
+    // pre C++ 14
+    std::unique_ptr<double[]> _pvalues { new double[n]};
+    // C++ 14+
+    auto _pvalues2{ std::make_unique<double[]>(n) };
+    
+    // reset the pointer
+    _pdata.reset();
+    _pvalues2.reset();
+    
+    // unique with nullptr
+    std::unique_ptr<int> _empty_value;
+    std::unique_ptr<int> _empty_value2 {};
+    std::unique_ptr<int> _empty_value3 {nullptr};
+    
+    // change smart ptr into raw ptr
+    int* leak = _empty_value.release();
+    
+    delete leak;
+    
+    // shared_ptr<T>
+    std::shared_ptr<double> _shr_pdata { new double{999.0} };
+    
+    // can dereference
+    *_shr_pdata = 8888.0;
+    std::cout << *_shr_pdata << std::endl;
+    
+    // more efficient
+    auto _shr_data { std::make_shared<double>(999.0) };
+    
+    std::shared_ptr<double> _shr_data2 {_shr_data};
+    
+    std::shared_ptr<double> _shr_data3;
+    _shr_data3 = _shr_data; // copy pointer
+    std::cout << *pdata3 << std::endl;
+    
+    // weak_ptr<T>
     
     return 0;
 }
